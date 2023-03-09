0f7165bd2ecac607466096683664d7b26bd0e75d
micheleTummolo <tummolomichele02@gmail.com>
2023-03-09 17:09:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import ProjectMain from './components/ProjectMain.vue'
+export default {
+  components: {
+    ProjectMain
+  }
+}
+</script>
+
+<template>
+  <ProjectMain></ProjectMain>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "ProjectMain",
+    data() {
+        return {
+            projects: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>ProjectMain</h1>
+</template>
+
+<style lang="scss" scoped></style>
