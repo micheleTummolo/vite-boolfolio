d6688962091534ab60fc07e10285fe21e71e4583
micheleTummolo <tummolomichele02@gmail.com>
2023-03-12 16:20:09 +0100

    Added route vuew and header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script>
-import ProjectMain from './components/ProjectMain.vue'
 export default {
   components: {
-    ProjectMain
+
   }
 }
 </script>
 
 <template>
-  <ProjectMain></ProjectMain>
+  <router-view></router-view>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'Homepage'
+}
+</script>
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-5">
+                <h1 class="fw-bolder">Homepage</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
